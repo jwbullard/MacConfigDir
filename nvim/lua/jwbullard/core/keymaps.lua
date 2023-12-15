ab9a18495db8d60aef40e6583319b96cadeb961d
Jeffrey W. Bullard <jwbullard@tamu.edu>
2023-12-15 13:51:27 -0600

    Added some keymaps for moving around buffers

--- nvim/lua/jwbullard/core/keymaps.lua
+++ nvim/lua/jwbullard/core/keymaps.lua
@@ -9,8 +9,9 @@ keymap.set("n", "x", '"_x')
 keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s") -- split window horzontally
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
-keymap.set("n", "<leader>bc", '"+yy') -- copy into computer's clipboard
-keymap.set("n", "<leader>bp", '"+p') -- paste from computer's clipboard
+keymap.set("n", "<leader>bn", ':bnext<CR>') -- copy into computer's clipboard
+keymap.set("n", "<leader>bp", ':bprev<CR>') -- copy into computer's clipboard
+keymap.set("n", "<leader>b", ':buffers<CR>:buffer<Space>') -- show buffers and get ready to specify one
 
 
 -- nvim-tree
