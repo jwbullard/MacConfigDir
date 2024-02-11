682cd5335ad93d9e242a4fdd19f6f7f976f6da8f
Jeffrey W. Bullard <jwbullard@tamu.edu>
2024-02-10 21:48:33 -0600

    Removed QtProject folder, fish folder, and configured neovide better

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,3 +4,5 @@ require("jwbullard.core.options")
 vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#969696", bold = false })
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#E1E1E1", bold = true })
 vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#969696", bold = false })
+
+vim.g.neovide_cursor_vfx_mode = "railgun"
