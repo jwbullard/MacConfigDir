921f94b953a80dedf9a94b06d17bd37a467dab05
Jeff Bullard <jwbullard@tamu.edu>
2024-04-05 11:40:40 -0500

    Second Neorg attempt seems to be working.
    Check the plugins/neorg.lua file for new configuration.

---
+++ nvim/lua/jwbullard/plugins/neorg.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- We'd like this plugin to load first out of the rest
+    config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+  },
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    -- put any other flags you wanted to pass to lazy here!
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {}, -- Loads Default behavior
+          ["core.concealer"] = {}, -- Loads Default behavior
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+            },
+          },
+        },
+      })
+    end,
+    run = ":Neorg sync-parsers",
+  },
+}
