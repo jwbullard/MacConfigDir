f35778220388ac698ad7dd80a27e117a561ec36f
Jeffrey W. Bullard <jwbullard@tamu.edu>
2024-04-21 22:01:44 -0500

    Changed colorscheme to kanagawa
    Can change back to mellifluous by (un)commenting the relevant code
    blocks in plugins/colorscheme.lua

--- nvim/lua/jwbullard/plugins/colorscheme.lua
+++ nvim/lua/jwbullard/plugins/colorscheme.lua
@@ -1,12 +1,30 @@
+--return {
+--  "ramojus/mellifluous.nvim",
+--  priority = 1000,
+--  config = function()
+-- local mellifluous = require("mellifluous")
+-- mellifluous.setup({
+-- dim_inactive = false,
+-- color_set = "mountain",
+-- })
+-- vim.cmd([[colorscheme mellifluous]])
+-- end,
+-- }
+
 return {
-  "ramojus/mellifluous.nvim",
+  "rebelot/kanagawa.nvim",
   priority = 1000,
   config = function()
-    local mellifluous = require("mellifluous")
-    mellifluous.setup({
-      dim_inactive = false,
-      color_set = "mountain",
+    local kanagawa = require("kanagawa")
+    kanagawa.setup({
+      dimInactive = false,
+      transparent = true,
+      theme = "wave",
+      background = {
+        dark = "wave",
+        light = "lotus",
+      },
     })
-    vim.cmd([[colorscheme mellifluous]])
+    vim.cmd([[colorscheme kanagawa]])
   end,
 }
