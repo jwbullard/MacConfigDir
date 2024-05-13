3c63fb8b08b2aa5b2aa3708d1eeafba78d8d27d5
Jeff Bullard <jwbullard@tamu.edu>
2024-05-13 10:50:09 -0500

    Added some gitsigns keymaps

--- nvim/lua/jwbullard/plugins/gitsigns.lua
+++ nvim/lua/jwbullard/plugins/gitsigns.lua
@@ -45,5 +45,7 @@ return {
         col = 1,
       },
     })
+    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
   end,
 }
