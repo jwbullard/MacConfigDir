097d5855d00cff01232a98c0ae8ae57a6f4a4bca
Jeff Bullard <jwbullard@tamu.edu>
2024-05-29 16:06:03 -0500

    Better integration with git functions

--- nvim/lua/jwbullard/plugins/bufferline.lua
+++ nvim/lua/jwbullard/plugins/bufferline.lua
@@ -1,6 +1,28 @@
 return {
   "akinsho/bufferline.nvim",
-  version = "*",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = true,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local bufferline = require("bufferline")
+    bufferline.setup({
+      options = {
+        mode = "buffers",
+        style_preset = bufferline.style_preset.default,
+      },
+    })
+  end,
 }
+-- {
+-- "NeogitOrg/neogit",
+-- dependencies = {
+-- "nvim-lua/plenary.nvim",
+-- "sindrets/diffview.nvim",
+-- "nvim-telescope/telescope.nvim",
+-- },
+-- config = function()
+-- local neogit = require("neogit")
+-- neogit.setup({})
+-- vim.keymap.set("n", "<leader>gs", neogit.open, { silent = true, noremap = true })
+-- end,
+-- },
--- nvim/lua/jwbullard/plugins/gitstuff.lua
+++ nvim/lua/jwbullard/plugins/gitstuff.lua
@@ -62,10 +62,6 @@ return {
       local neogit = require("neogit")
       neogit.setup({})
       vim.keymap.set("n", "<leader>gs", neogit.open, { silent = true, noremap = true })
-      vim.keymap.set("n", "<leader>gc", ":Neogit commit<CR>", { silent = true, noremap = true })
-      vim.keymap.set("n", "<leader>gp", ":Neogit pull<CR>", { silent = true, noremap = true })
-      vim.keymap.set("n", "<leader>gP", ":Neogit push<CR>", { silent = true, noremap = true })
-      vim.keymap.set("n", "<leader>gB", ":G blame<CR>", { silent = true, noremap = true })
     end,
   },
 }
