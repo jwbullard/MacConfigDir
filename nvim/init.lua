5725ea6a7ef9e085d9f9bb29f1f5110cc4b173a9
Jeffrey W. Bullard <jwbullard@tamu.edu>
2024-10-02 22:26:23 -0500

    Set global autoindent and added hlchunk plugin for cool chunk highlighting

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,5 +8,12 @@ vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#969696", bold = false })
 
 vim.g.neovide_cursor_vfx_mode = "railgun"
 
+-- Set global indenting and tab stops
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.cindent = true
+
 -- Load snippets from ~/.config/nvim/LuaSnip/
 require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/LuaSnip/" })
