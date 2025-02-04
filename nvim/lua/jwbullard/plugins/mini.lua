d619470b4658455ecb0759a6ef6bf6917a1bb9cd
Jeffrey W. Bullard <jwbullard@tamu.edu>
2025-02-04 12:30:56 -0600

    Added some modules from mini.nvim and snacks.nvim

---
+++ nvim/lua/jwbullard/plugins/mini.lua
@@ -0,0 +1,17 @@
+return {
+  "echasnovski/mini.nvim",
+  version = false,
+  config = function()
+    -- vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
+
+    require("mini.ai").setup()
+    require("mini.align").setup()
+    require("mini.pairs").setup()
+    require("mini.surround").setup() -- alternative to nvim-surround
+    require("mini.clue").setup()
+    require("mini.extra").setup()
+    -- require("mini.indentscope").setup() -- alternative to hlchunk
+    require("mini.jump").setup() -- jump to next character, etc.
+    require("mini.operators").setup() -- used for auto-sorting text
+  end,
+}
