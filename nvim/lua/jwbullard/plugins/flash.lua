a1f6cc114a107dee7c862a68e13bd71060d1074c
Jeffrey W. Bullard <jwbullard@tamu.edu>
2025-06-11 22:38:46 -0500

    Added search integration for flash plugin

--- nvim/lua/jwbullard/plugins/flash.lua
+++ nvim/lua/jwbullard/plugins/flash.lua
@@ -11,4 +11,24 @@ return {
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
+  config = function()
+    require("flash").setup({
+      modes = {
+        -- options used when flash is activated through
+        -- a regular search with `/` or `?`
+        search = {
+          -- when `true`, flash will be activated during regular search by default.
+          -- You can always toggle when searching with `require("flash").toggle()`
+          enabled = true,
+          highlight = { backdrop = false },
+          jump = { history = true, register = true, nohlsearch = true },
+          search = {
+            -- `forward` will be automatically set to the search direction
+            -- `mode` is always set to `search`
+            -- `incremental` is set to `true` when `incsearch` is enabled
+          },
+        },
+      },
+    })
+  end,
 }
